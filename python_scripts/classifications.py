86c69666f32987c1bf1d94023768cd4fefdd3f55
ianjjhsiao <ianjjhsiao@gmail.com>
2020-07-28 02:15:14 -0700

    Added epoch time to processed data

--- python_scripts/classifications.py
+++ python_scripts/classifications.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from joblib import dump, load
-
+import datetime
 
 #takes input files
 
@@ -26,9 +26,24 @@ def add_datetime_to_processed_data(proc_data):
     proc_data['datetime'] = (['time'], datetime)
     return proc_data
 
+def add_epoch_time_to_processed_data(proc_data):
+    """take the 2DS processed dataset and reindex the time properly"""
+    msec = proc_data.msec.values
+    secs = proc_data.Time.values%100
+    mins = (proc_data.Time.values//100)%100
+    hours = proc_data.Time.values//10000
+    
+    day = proc_data.Date.values%100
+    months = (proc_data.Date.values//100)%100
+    year = proc_data.Date.values//10000
+    epoch = np.array([datetime.datetime(year[i],months[i],day[i],hours[i],mins[i],secs[i],msec[i]*1000).timestamp() for i in range(len(year))])
+    proc_data['epoch'] = (['time'], epoch)
+    return proc_data
+
 #formats data and splits it into good and bad (for the model)
 
 data = add_datetime_to_processed_data(data)
+data = add_epoch_time_to_processed_data(data)
 data["area_ratio"] = data.image_area / (np.pi/4 * data.image_diam_minR ** 2)
 iat = np.insert(np.diff(data['Time_in_seconds'].values), 0, .001)
 data['log10_iat'] = (('time',), np.log10(iat))
