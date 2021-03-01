30c3c909c7b65a5d91ddce5b01c31f35bb7f433d
Johannes Mohrmann <jkcm@uw.edu>
2021-03-01 13:52:03 -0800

    edited run_psd to allow for importing of its functions

--- python_scripts/run_psd.py
+++ python_scripts/run_psd.py
@@ -42,23 +42,25 @@ def bootstrap_ml_classifications(cats, certs, n_iters=1):
     realizations = (np.random.rand(*flipped_certs.shape) < flipped_certs).astype(int)
     return realizations
     
-all_flights = [f'rf{i:02}' for i in np.arange(1,16)]
-for flt_string in all_flights:
-    try:
-        outfile = f'/home/disk/eos9/jkcm/Data/particle/psd/test/{flt_string}_psd.nc'
-        navfilename = glob.glob('/home/disk/eos9/jfinlon/socrates/' + flt_string + '/*.PNI.nc')[0]
-        pbpfilename = '/home/disk/eos9/jfinlon/socrates/' + flt_string + '/pbp.' + flt_string + '.2DS.H.nc'
-        phasefilename = '/home/disk/eos9/jkcm/Data/particle/classified/' + 'UW_particle_classifications.' + flt_string + '.nc'
-        [flt_time, flt_tas] = load_nav(navfilename)
-        [time, diam_minR, diam_areaR, phase_ml, phase_holroyd, phase_ar, prob_ml, time_all, intArr, ovrld_flag] = load_pbp(
-            pbpfilename, phasefilename, Dmin=0.05, Dmax=3.2, iatThresh=1.e-6)
+if __name__ == "__main__":
 
-        psd = make_psd(flt_time, flt_tas, time, diam_minR, diam_areaR, phase_ml, prob_ml, phase_holroyd, phase_ar,
-          time_all, intArr, ovrld_flag, bootstrap=True, binEdges=None, tres=1, outfile=outfile)
-    except KeyboardInterrupt as e:
-        raise e
-    except Exception as e:
-        print(f'error on flight {flt_string}:')
-        print(e)
-        continue
-        
+    all_flights = [f'rf{i:02}' for i in np.arange(1,16)]
+    for flt_string in all_flights:
+        try:
+            outfile = f'/home/disk/eos9/jkcm/Data/particle/psd/test/{flt_string}_psd.nc'
+            navfilename = glob.glob('/home/disk/eos9/jfinlon/socrates/' + flt_string + '/*.PNI.nc')[0]
+            pbpfilename = '/home/disk/eos9/jfinlon/socrates/' + flt_string + '/pbp.' + flt_string + '.2DS.H.nc'
+            phasefilename = '/home/disk/eos9/jkcm/Data/particle/classified/' + 'UW_particle_classifications.' + flt_string + '.nc'
+            [flt_time, flt_tas] = load_nav(navfilename)
+            [time, diam_minR, diam_areaR, phase_ml, phase_holroyd, phase_ar, prob_ml, time_all, intArr, ovrld_flag] = load_pbp(
+                pbpfilename, phasefilename, Dmin=0.05, Dmax=3.2, iatThresh=1.e-6)
+
+            psd = make_psd(flt_time, flt_tas, time, diam_minR, diam_areaR, phase_ml, prob_ml, phase_holroyd, phase_ar,
+            time_all, intArr, ovrld_flag, bootstrap=True, binEdges=None, tres=1, outfile=outfile)
+        except KeyboardInterrupt as e:
+            raise e
+        except Exception as e:
+            print(f'error on flight {flt_string}:')
+            print(e)
+            continue
+            
